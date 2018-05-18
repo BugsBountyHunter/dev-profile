1b2e52fd387e4104682ba2ca6880a6bbac7e808c
Admin <Ahmed@admins-mac.local>
2018-05-18 15:47:09 +0300

    Initial Commit

---
+++ dev-profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Ahmed on 9/3/1439 AH.
+//  Copyright © 1439 AH Steph_saber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
